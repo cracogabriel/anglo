c906cc366d9fe9c56b38771614f2f0222831f44b
craco <90925313+GabrielCraco@users.noreply.github.com>
2024-03-12 11:21:27 -0300

    refactor: change theme

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -6,7 +6,7 @@ import { FirstComponentComponent } from '../first-component/first-component.comp
 import { InputComponent } from '../input/input.component';
 import { FormBuilder } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { Todo } from '../../interfaces/todo';
 @Component({
   selector: 'app-todo-list',
   standalone: true,
@@ -28,20 +28,47 @@ export class TodoListComponent {
   ) {
     this.todos = this.listService.todos;
   }
-  todos: string[];
+  todos: Todo[];
   todoForm = this.formBuilder.group({
-    todo: '',
+    name: '',
+    expires_in: null,
+    description: null,
   });
 
+  formatDate(date: Date) {
+    return new Date(date).toLocaleDateString('pt-BR');
+  }
+
+  getTodoValidation(date: Date) {
+    if (new Date(date) < new Date()) return 'tomato';
+    else if (new Date(date) === new Date()) return 'orange';
+    else return '#0d3ba5';
+  }
+
   submitToDo() {
-    if (!this.todoForm.value.todo) return;
-    this.listService.addTodo(this.todoForm.value.todo);
+    if (!this.todoForm.value.name) return;
+
+    let date = this.todoForm.value.expires_in
+      ? new Date(this.todoForm.value.expires_in)
+      : null;
+
+    let offset = 3.0;
+    let userDate = date
+      ? new Date(date.getTime() + offset * 3600 * 1000)
+      : null;
+
+    this.listService.addTodo({
+      id: new Date().getTime(),
+      name: this.todoForm.value.name,
+      expires_in: userDate,
+      description: this.todoForm.value.description,
+    });
     this.todos = this.listService.todos;
     this.todoForm.reset();
   }
 
-  removeToDo(todo: string) {
-    this.listService.removeTodo(todo);
+  removeToDo(id: number) {
+    this.listService.removeTodo(id);
     this.todos = this.listService.todos;
   }
 }
